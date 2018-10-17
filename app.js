09a161263a9cb6b456a608d5978e43806ec720bc
Konrad <konrad1997@gmail.com>
2018-10-17 19:21:35 +0200

    added playing music

--- app.js
+++ app.js
@@ -313,7 +313,27 @@ client.on('message', message => {
         }
     }
     else if (message.content === '::skip'){
-        //TODO: implement skipping currently playing song
+        if (message.guild.voiceConnection){
+            if (servers[message.guild.id]){
+                servers[message.guild.id].dispatcher.end()
+                const embed = new RichEmbed()
+                    .setColor('#00FF00')
+                    .setDescription("Skipped currently playing song")
+                message.channel.send(embed)
+            }
+            else{
+                const embed = new RichEmbed()
+                    .setColor('#FF0000')
+                    .setDescription("No song is currently playing")
+                message.channel.send(embed)
+            }
+        }
+        else{
+            const embed = new RichEmbed()
+                .setColor('#FF0000')
+                .setDescription("Bot must be in a voice channel")
+            message.channel.send(embed)
+        }
     }
 })
 
