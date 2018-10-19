03d00e93609c2042f640ee68705701ce78ad5334
Konrad <konrad1997@gmail.com>
2018-10-19 23:16:31 +0200

    cleanup

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-const valid = require('valid-url');
-
-const { RichEmbed } = require('discord.js');
 const commando = require('discord.js-commando');
 const mongoose = require('mongoose');
-const Reaction = require('./Data/Schema/reaction.js');
 const express = require('express');
 const https = require('https');
-const YTDL = require('ytdl-core');
 
 const app = express();
-
 app.listen(process.env.PORT || 8080);
 
-mongoose.connect("mongodb://@ds139187.mlab.com:39187/reactbot-db", {
-    'user': process.env.DBUSER,
-    'pass': process.env.DBPASS,
+global.servers = {};
+global.connections = {};
+
+mongoose.connect(process.env.DB_URL, {
+    'user': process.env.DB_USER,
+    'pass': process.env.DB_PASS,
     'useNewUrlParser': true,
 });
-
 const db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
     console.log('Connection open')
@@ -29,21 +24,14 @@ const client = new commando.Client({
     commandPrefix: '?',
     unknownCommandResponse: false
 });
-
-global.servers = {};
-global.connections = {};
-
-client.registry
-    .registerGroup('basic', 'Basic')
+client.registry.registerGroup('basic', 'Basic')
     .registerGroup('music', 'Music')
     .registerCommandsIn(__dirname + "/commands");
-
 client.on('ready', () => {
     console.log('Ready!');
     client.user.setGame('?h for help');
     setInterval(() => {
-        https.get('https://discord-react-bot-bodemouf.herokuapp.com/')
+        https.get(process.env.APP_URL)
     }, 100000);
 });
-
 client.login(process.env.BOT_TOKEN);
