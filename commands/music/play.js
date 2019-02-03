8c628a3fd1f88169a58ac0d365a0a8fa17b2815e
Konrad <konrad1997@gmail.com>
2019-02-03 03:37:09 +0100

    Polish code

--- commands/music/play.js
+++ commands/music/play.js
@@ -28,30 +28,35 @@ class PlayCommand extends commando.Command {
         let server = servers[message.guild.id];
         let connection = connections[message.guild.id];
 
-        let url = server.queue[0];
+        let info = server.queue[0];
 
-        let video = YTDL.validateURL(url)
-            ? YTDL(url, {filter: "audioonly", quality: "highestaudio"})
-            : YoutubeDL(url, ['-q', '--no-warnings', '--force-ipv4', '--restrict-filenames'], undefined);
+        let video = YTDL.validateURL(info.url)
+            ? YTDL(info.url, {filter: "audioonly", quality: "highestaudio"})
+            : YoutubeDL(info.url, ['-q', '--no-warnings', '--force-ipv4', '--restrict-filenames'], undefined);
 
         server.dispatcher = connection.playStream(video);
 
-        YoutubeDL.getInfo(url, ['-q', '--no-warnings', '--force-ipv4', '--restrict-filenames'], null, (err, info) => {
-            if (server.queue){
-                if (info){
-                    const embed = new RichEmbed()
-                        .setColor('#00FF00')
-                        .setDescription("Playing **" + info.title + "**");
-                    message.channel.send(embed);
-                }
-                else{
-                    const embed = new RichEmbed()
-                        .setColor('#FF0000')
-                        .setDescription("Error getting song info");
-                    message.channel.send(embed);
-                }
-            }
-        });
+        // YoutubeDL.getInfo(url, ['-q', '--no-warnings', '--force-ipv4', '--restrict-filenames'], null, (err, info) => {
+        //     if (server.queue){
+        //         if (info){
+        //             const embed = new RichEmbed()
+        //                 .setColor('#00FF00')
+        //                 .setDescription("Playing **" + info.title + "**");
+        //             message.channel.send(embed);
+        //         }
+        //         else{
+        //             const embed = new RichEmbed()
+        //                 .setColor('#FF0000')
+        //                 .setDescription("Error getting song info");
+        //             message.channel.send(embed);
+        //         }
+        //     }
+        // });
+
+        const embed = new RichEmbed()
+            .setColor('#00FF00')
+            .setDescription("Playing **" + info.title + "**");
+        message.channel.send(embed);
 
         server.queue.shift();
 
@@ -86,7 +91,7 @@ class PlayCommand extends commando.Command {
                 YoutubeDL.getInfo(url, ['-q', '--no-warnings', '--force-ipv4', '--restrict-filenames'], null, (err, info) => {
                     if (info){
                         if (servers[message.guild.id]) {
-                            servers[message.guild.id].queue.push(url);
+                            servers[message.guild.id].queue.push(info);
                             const embed = new RichEmbed()
                                 .setColor('#00FF00')
                                 .setDescription("Added **" + info.title + "** to the queue");
@@ -94,7 +99,7 @@ class PlayCommand extends commando.Command {
                         }
                         else {
                             servers[message.guild.id] = {queue: []};
-                            servers[message.guild.id].queue.push(url);
+                            servers[message.guild.id].queue.push(info);
                             this._playFunc(message);
                         }
                     }
