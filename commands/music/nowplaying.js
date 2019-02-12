f306aa79221b134b57229390c3c1165987cbf7ed
Konrad <konrad1997@gmail.com>
2019-02-12 18:25:15 +0100

    Add loop function

--- commands/music/nowplaying.js
+++ commands/music/nowplaying.js
@@ -1,5 +1,5 @@
 const commando = require('discord.js-commando');
-const {sendError, songTime} = require('../../utils.js');
+const {sendOk, sendError, songTime} = require('../../utils.js');
 const {RichEmbed} = require('discord.js');
 
 class NowPlayingCommand extends commando.Command {
@@ -21,17 +21,13 @@ class NowPlayingCommand extends commando.Command {
                 let title = server.nowplaying.title;
                 let length = server.nowplaying.duration;
                 let url = server.nowplaying.url;
-                let description = `Current time: \`${songTime(time, length)}\``;
+                let description = `\nCurrent time: \`${songTime(time, length)}\``;
 
                 if (server.nowplaying.loop){
                     description += '\nLooped'
                 }
 
-                const embed = new RichEmbed()
-                    .setTitle(`Now playing: [${title}](${url})`)
-                    .setColor('#00FF00')
-                    .setDescription(description);
-                message.channel.send(embed);
+                sendOk(message, `Now playing: [${title}](${url})` + description);
             }
             else {
                 sendError(message, "No song is currently playing");
