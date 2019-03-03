f2c7e3ceb938be2f5c37a4c56f6d409d2e73feaf
Konrad <konrad1997@gmail.com>
2019-03-03 02:39:53 +0100

    Edit help message

--- commands/core/help.js
+++ commands/core/help.js
@@ -23,7 +23,9 @@ class HelpCommand extends commando.Command {
             .addField("```!leave```", "Leaves voice channel.")
             .addField("```!play <URL | Query>```", "Plays song from **URL** or plays first song from YouTube based on **Query**")
             .addField("```!loop```", "Loops (or unloops) currently playing song")
+            .addField("```!playlist```", "Manages playlists")
             .addField("```!nowplaying```", "Shows information about the currently playing song")
+            .addField("```!queue```", "Shows song queue")
             .addField("```!skip```", "Skips currently playing song.");
         message.channel.send(embed);
 
