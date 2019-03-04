bde275081bd053f0282863e6e09ddd956ca8e33c
Konrad <konrad1997@gmail.com>
2019-03-04 23:04:53 +0100

    Refactor LoopCommand

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -12,24 +12,25 @@ class LoopCommand extends commando.Command {
     }
 
     async run(message) {
-        if (message.guild.voiceConnection) {
+        if (!message.guild.voiceConnection) {
+            sendError(message, "**Bot must be in a voice channel**");
+        }
+        else {
             let server = servers[message.guild.id];
+
             if (server) {
-                if (server.nowplaying.loop){
+                if (server.nowplaying.loop) {
                     server.nowplaying.loop = false;
                     sendOk(message, "**Unlooping currently playing song**");
-                } else{
+                } else {
                     server.nowplaying.loop = true;
                     sendOk(message, "**Looping currently playing song**");
                 }
 
             }
             else {
-                sendError(message, "**No song is currently playing**");
+                sendError(message, "**No song is currently being played**");
             }
-        }
-        else {
-            sendError(message, "**Bot must be in a voice channel**");
         }
 
     }
