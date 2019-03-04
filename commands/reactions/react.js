d6f17a9ffcc71a2a9b0be8bd1099c6e6898b9e32
Konrad <konrad1997@gmail.com>
2019-03-04 22:57:08 +0100

    Refactor ReactCommand

--- commands/reactions/react.js
+++ commands/reactions/react.js
@@ -12,7 +12,7 @@ class ReactCommand extends commando.Command {
             argsPromptLimit: 0,
             description: 'Reacts with specified reaction',
             aliases: ['r'],
-            args:[
+            args: [
                 {
                     key: 'name',
                     type: 'string',
@@ -30,21 +30,21 @@ class ReactCommand extends commando.Command {
             sendError(message, "**Specify reaction name**");
         }
         else {
-            Reaction.find({'name': name}, 'name url', (err, response) => {
+            Reaction.findOne({'name': name}, (err, response) => {
                 if (err) {
                     console.log(err.content);
                     sendError("**Something went wrong, try again or specify a different reaction**");
                 }
-                else if (response.length) {
-                    const embed = new RichEmbed()
-                        .setColor('#00FF00')
-                        .setTitle(message.member.user.tag + ' reacts with ' + response[0].name)
-                        .setImage(response[0].url);
-                    message.channel.send(embed);
-                    message.delete(100);
+                else if (!response) {
+                    sendError(message, "**Reaction does not exist**");
                 }
                 else {
-                    sendError(message, "**Reaction does not exist**");
+                    const embed = new RichEmbed()
+                        .setColor('#00FF00')
+                        .setTitle(message.member.user.tag + ' reacts with ' + response.name)
+                        .setImage(response.url);
+                    message.channel.send(embed);
+                    message.delete(100);
                 }
             });
         }
